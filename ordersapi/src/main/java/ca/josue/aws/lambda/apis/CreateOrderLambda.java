eacf4dbcabdec8a35a4522323921554bc99d1bba
Josue Lubaki <josuelubaki@gmail.com>
2022-05-31 13:55:54 -0400

    created DynamoDB database

--- ordersapi/src/main/java/ca/josue/aws/lambda/apis/CreateOrderLambda.java
+++ ordersapi/src/main/java/ca/josue/aws/lambda/apis/CreateOrderLambda.java
@@ -1,6 +1,10 @@
 package ca.josue.aws.lambda.apis;
 
 import ca.josue.aws.lambda.apis.dto.Order;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -10,6 +14,21 @@ public class CreateOrderLambda {
     public APIGatewayProxyResponseEvent createOrder(APIGatewayProxyRequestEvent request) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         Order order = objectMapper.readValue(request.getBody(), Order.class);
+
+        // initialize dynamoDB client
+        DynamoDB dynamoDB = new DynamoDB(AmazonDynamoDBClientBuilder.defaultClient());
+
+        // create table if not exists
+        Table orders_table = dynamoDB.getTable(System.getenv("ORDERS_TABLE"));
+
+        // create item
+        Item item = new Item()
+                .withPrimaryKey("id", order.id)
+                .withString("itemName", order.itemName)
+                .withInt("quantity", order.quantity);
+
+        // save item
+        orders_table.putItem(item);
 
         return new APIGatewayProxyResponseEvent().withStatusCode(201).withBody("Order ID : " + order.id);
     }
