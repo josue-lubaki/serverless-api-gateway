88bc3a07e425957af7ecd9fe19b50941e56b11af
Josue Lubaki <josuelubaki@gmail.com>
2022-05-31 12:08:43 -0400

    added CreateOrderLambda method

---
+++ ordersapi/src/main/java/ca/josue/aws/lambda/apis/CreateOrderLambda.java
@@ -0,0 +1,16 @@
+package ca.josue.aws.lambda.apis;
+
+import ca.josue.aws.lambda.apis.dto.Order;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class CreateOrderLambda {
+    public APIGatewayProxyResponseEvent createOrder(APIGatewayProxyRequestEvent request) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Order order = objectMapper.readValue(request.getBody(), Order.class);
+
+        return new APIGatewayProxyResponseEvent().withBody("Order ID : " + order.id);
+    }
+}
---
+++ ordersapi/src/main/java/ca/josue/aws/lambda/apis/dto/Order.java
@@ -0,0 +1,16 @@
+package ca.josue.aws.lambda.apis.dto;
+
+public class Order {
+    public int id;
+    public String itemName;
+    public int quantity;
+
+    public Order() {
+    }
+
+    public Order(int id, String itemName, int quantity) {
+        this.id = id;
+        this.itemName = itemName;
+        this.quantity = quantity;
+    }
+}
